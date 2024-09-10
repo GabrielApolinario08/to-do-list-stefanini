bd345c51cac181b816d4f459ef1f12732981ce9f
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-09-10 14:43:41 -0300

    primeiro commit

---
+++ backend/src/main/java/com/example/to_do_list_backend/ToDoListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.to_do_list_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/to_do_list_backend/ToDoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_do_list_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
