0e9f8a1fbd25dffef04dafb919ea93c21ab3185e
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-09-11 12:48:30 -0300

    criando estrutura básica e estilizando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,13 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-    
+    <div className='container'>
+      <div className='container-tarefas'>
+        <h1>Lista de Tarefas</h1>
+        <div className='tarefas'>
+          
+        </div>
+      </div>
     </div>
   )
 }
